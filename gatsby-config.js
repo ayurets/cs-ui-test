1d29b49960b05c6e59f8e3f349642c37d7c0683b
Andrii Yurets <ayurets@tacitknowledge.com>
2023-05-02 18:03:12 -0500

    CS-UI: install and config gatsby-source-contentstack

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `Playground`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ["gatsby-plugin-styled-components", {
+    resolve: `gatsby-source-contentstack`,
+    options: {
+      // Required: API Key is a unique key assigned to each stack.
+      api_key: `blt9fe7ff0979b8c046`,
+
+      // Required: Delivery Token is a read-only credential. 
+      delivery_token: `cs56bf5d5b80f2cea1afb65edd`,
+      
+      // Required: Environment where you published your data.
+      environment: `development`,
+    },
+  },]
 };
