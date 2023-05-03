54e24e0648ed24dff12dea6f95823c23f913b5e0
Andrii Yurets <ayurets@tacitknowledge.com>
2023-05-03 12:52:06 -0500

    CS-UI: update CS config for prod | dev env

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 const pageStyles = {
   color: "#232129",
@@ -7,9 +8,55 @@ const pageStyles = {
 }
 
 const IndexPage = () => {
+
+  const data = useStaticQuery(graphql`
+  query {
+    contentstackHeader{
+       logo {
+          url
+        },
+      navigation_menu {
+        label
+        page_reference {
+          uid
+          title
+          created_by
+          page_components {
+            hero_banner{
+              bg_color,
+              text_color
+              banner_title
+              banner_image {
+                id
+              }
+              banner_description
+              call_to_action {
+                title
+                href
+              }
+            },
+          }
+        }
+      }
+    }
+  }
+  `)
+
+  React.useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
     <main style={pageStyles}>
     <h1>Hello World</h1>
+    <img src={data.contentstackHeader.logo.url} ></img>
+    <ul>
+      {
+        data.contentstackHeader.navigation_menu.map(({label}) => {
+          return<li><span>{label}</span></li>
+        })
+      }
+    </ul>
     </main>
   )
 }
